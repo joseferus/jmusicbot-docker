ad85478370190a939c3a8e610b3029f4e7cec147
Josef Bertolini <joe.bertolini.real@gmail.com>
2026-08-23 23:21:22 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,29 +3,30 @@
 # Note: the image this container is based off of doesn't include `sudo`, but anytime I've done something in the container, I was root
 # Note: this image provided a wrapper around `apt` called `install_packages`, it's being used here
 
-echo -e "Downloading dependencies to install docker..."
+echo "Downloading dependencies to install docker..."
 apt update
 install_packages ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
-echo -e "Dependencies download, certificates configured"
+echo "Dependencies download, certificates configured"
 
-echo -e "Adding the Docker repository to the apt sources list"
+echo "Adding the Docker repository to the apt sources list"
 touch /etc/apt/sources.list.d/docker.sources 
 echo "Types: deb" >> /etc/apt/sources.list.d/docker.sources 
 ehco "URIs: https://download.docker.com/linux/ubuntu" >> /etc/apt/sources.list.d/docker.sources  
 echo "Suites: bullseye)" >> /etc/apt/sources.list.d/docker.sources 
 echo "Components: stable" >> /etc/apt/sources.list.d/docker.sources 
-echo "Architectures: $(dpkg --print-architecture)" >> /etc/apt/sources.list.d/docker.sources 
+echo "Architectures: amd64" >> /etc/apt/sources.list.d/docker.sources 
 echo "Signed-By: /etc/apt/keyrings/docker.asc" >> /etc/apt/sources.list.d/docker.sources 
+
 apt update
 if [ $? -ne 0 ]; then
-	echo -e "Adding to apt sources failed"
+	echo "Adding to apt sources failed"
 	exit 1
 fi
 
-echo -e "Installing Docker"
+echo "Installing Docker"
 install_packages docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 command -v docker
