2c07af0993f96b325492669a6a28f76c0f2fea62
Josef Bertolini <joe.bertolini.real@gmail.com>
2026-08-23 23:10:00 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -4,6 +4,7 @@
 # Note: this image provided a wrapper around `apt` called `install_packages`, it's being used here
 
 echo -e "Downloading dependencies to install docker..."
+apt update
 install_packages ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
@@ -11,10 +12,14 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo -e "Dependencies download, certificates configured"
 
 echo -e "Adding the Docker repository to the apt sources list"
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bullseye stable" | \
-	tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-
+touch /etc/apt/sources.list.d/docker.sources 
+echo "Types: deb" >> /etc/apt/sources.list.d/docker.sources 
+ehco "URIs: https://download.docker.com/linux/ubuntu" >> /etc/apt/sources.list.d/docker.sources  
+echo "Suites: bullseye)" >> /etc/apt/sources.list.d/docker.sources 
+echo "Components: stable" >> /etc/apt/sources.list.d/docker.sources 
+echo "Architectures: $(dpkg --print-architecture)" >> /etc/apt/sources.list.d/docker.sources 
+echo "Signed-By: /etc/apt/keyrings/docker.asc" >> /etc/apt/sources.list.d/docker.sources 
+apt update
 if [ $? -ne 0 ]; then
 	echo -e "Adding to apt sources failed"
 	exit 1
@@ -22,4 +27,14 @@ fi
 
 echo -e "Installing Docker"
 install_packages docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+command -v docker
+if [ $? -ne 0 ]; then
+	echo "Docker not found"
+	exit 1
+fi
+
 systemctl start docker
+
+echo "Running Docker hello world"
+docker run hello-world
