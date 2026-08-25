f154aaac406cf8a72f58b2f2cc11f0b911a05dab
Josef Bertolini <joe.bertolini.real@gmail.com>
2026-08-24 22:33:40 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,7 @@
 
 echo "Downloading dependencies to install docker..."
 apt update
-install_packages ca-certificates curl systemctl
+install_packages ca-certificates curl systemctl procps
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
