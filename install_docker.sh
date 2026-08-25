3da8988cf2240c606ae7cfcfbca0ab830c7a0d45
Josef Bertolini <joe.bertolini.real@gmail.com>
2026-08-24 22:41:08 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,7 @@
 
 echo "Downloading dependencies to install docker..."
 apt update
-install_packages ca-certificates curl systemctl procps
+apt-get install ca-certificates curl systemctl procps
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -27,7 +27,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Installing Docker"
-install_packages docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 command -v docker
 if [ $? -ne 0 ]; then
